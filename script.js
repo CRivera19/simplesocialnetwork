318e49c81c050d16b4dfae277b70f3715802af29
Ctrivera19 <ctrivera19@gmail.com>
2019-07-08 20:09:14 -0700

    added user form.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// Define UI Vars
+const logInForm = document.querySelector('#logInForm');
+const welcome = document.querySelector('#welcome');
+const thoughtForm = document.querySelector('#form');
+const timeLine = document.querySelector('#timeline');
+const userThought = document.querySelector('#userThought');
+
+// Load all event listeners
+loadAllEventListeners();
+
+// Load all event listeners
+function loadAllEventListeners() {
+  // add thought event
+  thoughtForm.addEventListener('submit', addThought);
+}
+
+// Add Thought
+function addThought(e) {
+  if(userThought.value === '') {
+    alert('Add a thought');
+  }
+  e.preventDefault();
+}
+
 let database = [
   {
     username: "Chris",
@@ -15,6 +39,7 @@ let newsfeed = [
     timeline: "I am also tired."
   }
 ];
+
 
 function isUserValid(username, password) {
   for (let i = 0; i < database.length; i++) {
@@ -36,7 +61,11 @@ function signIn(username, password) {
   }
 }
 
-let userNamePrompt = prompt("What's your username?");
-let passwordPrompt = prompt("What's your password?");
+// let userNamePrompt = prompt("What's your username?");
+// let passwordPrompt = prompt("What's your password?");
 
-signIn(userNamePrompt, passwordPrompt);
+// signIn(userNamePrompt, passwordPrompt);
+
+
+// user types in what is on their mind and shows up on timeline. 
+
